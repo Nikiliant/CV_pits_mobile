7365fb257f057b347c0238a0c1c5b7fb217aee32
Nikiliant <varley193@mail.ru>
2017-09-02 16:18:54 +0300

    1

---
+++ app/.externalNativeBuild/cmake/release/armeabi/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-4.11.12-200.fc25.x86_64")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "4.11.12-200.fc25.x86_64")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/niki/Android/Sdk/ndk-bundle/build/cmake/android.toolchain.cmake")
+
+set(CMAKE_SYSTEM "Android-14")
+set(CMAKE_SYSTEM_NAME "Android")
+set(CMAKE_SYSTEM_VERSION "14")
+set(CMAKE_SYSTEM_PROCESSOR "armv5te")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
---
+++ app/src/main/java/com/example/niki/yumdetect/MainActivity.java
@@ -0,0 +1,139 @@
+package com.example.niki.yumdetect;
+
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import java.io.IOException;
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.Path;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    Uri mUri;
+    Bitmap mainImage;
+    Bitmap histroImage;
+
+    ImageView mainImageView;
+    LinearLayout histoImageView;
+    Bitmap bi = null;
+    boolean isColored;
+
+    LinearLayout view;
+    LinearLayout view_color;
+
+    boolean flag;
+    private int SIZE = 256;
+    private int NUMBER_OF_COLOURS = 3;
+
+    public final int RED = 0;
+    public final int GREEN = 1;
+    public final int BLUE = 2;
+
+    private int[][] colourBins;
+    private volatile boolean loaded = false;
+    private int maxY;
+
+    private static final int LDPI = 0;
+    private static final int MDPI = 1;
+    private static final int TVDPI = 2;
+    private static final int HDPI = 3;
+    private static final int XHDPI = 4;
+
+    float offset = 1;
+
+
+    private static final int PHOTO_INTENT_REQUEST_CODE=100;
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+
+        // Example of a call to a native method
+        //TextView tv = (TextView) findViewById(R.id.sample_text);
+        //tv.setText(stringFromJNI());
+    }
+
+
+
+    public void onclick (View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, mUri);
+        startActivityForResult(intent, PHOTO_INTENT_REQUEST_CODE);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
